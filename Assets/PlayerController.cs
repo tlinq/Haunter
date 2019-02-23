1c199015306aa6b6835f1b5e51c67c2ad7e28b97
tlinq <lindqvist.tobias.erik@gmail.com>
2019-02-23 12:55:03 +0000

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public Rigidbody2D rb;
+    public List<string> stringList;
+
+    public Vector2 moveDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody2D>();
+        stringList = new List<string>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInput();
+    }
+
+    void HandleInput() {
+        if (Input.GetKey(KeyCode.W)) {
+            moveDirection = new Vector2(0, 1);
+            rb.velocity = moveDirection * speed;
+        } else if (Input.GetKey(KeyCode.S)) {
+            moveDirection = new Vector2(0, -1);
+            rb.velocity = moveDirection * speed;
+        } else if (Input.GetKey(KeyCode.D)) {
+            moveDirection = new Vector2(1, 0);
+            rb.velocity = moveDirection * speed;
+        } else if (Input.GetKey(KeyCode.A)) {
+            moveDirection = new Vector2(-1, 0);
+            rb.velocity = moveDirection * speed;
+        } else {
+            moveDirection = new Vector2(0, 0);
+            rb.velocity = moveDirection * speed;
+        }
+
+    }
+}
